1e87c6f61bd3e46750bdd441b845b788ffc526bc
Nikhil <kumarnikhil48578@gmail.com>
2025-11-30 22:02:38 +0530

    feat: setup database

--- agent.ts
+++ agent.ts
@@ -1,7 +1,15 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { MessagesAnnotation } from "@langchain/langgraph";
+import { initDB } from "./db";
 
+/**
+ * Init database
+ */
+const database = initDB("./expenses.db")
 
+/**
+ * Initialise the LLM
+ */
 const model = new ChatOpenAI({
     model: "gpt-4.1",
 });
---
+++ db.ts
@@ -0,0 +1,19 @@
+import { DatabaseSync } from "node:sqlite";
+
+export  function initDB(dbPath: string): DatabaseSync {
+    
+    const database = new DatabaseSync(dbPath);
+
+    const query = `
+    CREATE TABLE IF NOT EXISTS expense (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        amount REAL NOT NULL,
+        date TEXT NOT NULL
+    )
+    `
+
+    database.exec(query);
+
+    return database;
+}

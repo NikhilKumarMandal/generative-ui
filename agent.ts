d8e45a52245a066b58ec637c62893fc5fe763d6f
Nikhil <kumarnikhil48578@gmail.com>
2025-12-01 20:51:07 +0530

    feat: compile graph

--- agent.ts
+++ agent.ts
@@ -1,8 +1,9 @@
 import { ChatOpenAI } from "@langchain/openai";
-import { MessagesAnnotation } from "@langchain/langgraph";
+import { MemorySaver, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import { initDB } from "./db";
 import { initTools } from "./tools";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
+import type { AIMessage } from "langchain";
 
 /**
  * Init database
@@ -22,9 +23,12 @@ const model = new ChatOpenAI({
  * Tool Node
  */
 
-const tooleNode = new ToolNode(tools)
+const toolsNode = new ToolNode(tools)
 
 
+/**
+ * Call Model node
+ */
 async function callModel(state: typeof MessagesAnnotation.State) {
 
     const llmWithTools = model.bindTools(tools);
@@ -42,3 +46,66 @@ async function callModel(state: typeof MessagesAnnotation.State) {
 
     return { messages: [response]}
 };
+
+/**
+ * Conditional edge
+ */
+function shouldContinue(
+    state: typeof MessagesAnnotation.State
+) {
+    const messages = state.messages;
+    const lastMessage = messages.at(-1) as AIMessage;
+
+    if (lastMessage.tool_calls?.length) {
+        return 'tools';
+    }
+
+    return '__end__';
+}
+
+function shouldCallModel(
+    state: typeof MessagesAnnotation.State
+) {
+    // todo: change this when chart tool will be implemented
+    return 'callModel';
+}
+
+
+
+/**
+ * Graph
+ */
+
+const graph = new StateGraph(MessagesAnnotation)
+    .addNode('callModel', callModel)
+    .addNode('tools', toolsNode)
+    .addEdge('__start__', 'callModel')
+    .addConditionalEdges('callModel', shouldContinue, {
+        __end__: '__end__',
+        tools: 'tools',
+    })
+    .addConditionalEdges('tools', shouldCallModel, {
+        callModel: 'callModel',
+    });
+
+const agent = graph.compile({
+    checkpointer: new MemorySaver(),
+});
+
+async function main() {
+    const response = await agent.invoke(
+        {
+            messages: [
+                {
+                    role: 'user',
+                    content: 'How much i have spent this month?',
+                },
+            ],
+        },
+        { configurable: { thread_id: '1' } }
+    );
+
+    console.log(JSON.stringify(response, null, 2));
+}
+
+main();
--- tools.ts
+++ tools.ts
@@ -4,22 +4,31 @@ import { DatabaseSync } from "node:sqlite";
 
 export function initTools(database: DatabaseSync) {
     
+    /**
+   * AddExpense tool
+   */
     const addExpense = tool(
         ({ title, amount }) => {
-            console.log({ title, amount });
+            // todo: do proper args validation
+            const date = new Date().toISOString().split('T')[0];
 
+            const stmt = database.prepare(
+                `INSERT INTO expenses (title, amount, date) VALUES (?, ?, ?)`
+            );
+            stmt.run(title, amount, date);
 
-            return JSON.stringify({ status: "success!" });
+            return JSON.stringify({ status: 'success!' });
         },
         {
-            name: "add_expense",
-            description: "Add the given expense to database",
+            name: 'add_expense',
+            description: 'Add the given expense to database',
             schema: z.object({
-                title: z.string().describe("The expense title"),
-                amount: z.number().describe("The amount spent")
+                title: z.string().describe('The expense title'),
+                amount: z.number().describe('The amount spent'),
             }),
         }
     );
+
 
     return [
         addExpense
